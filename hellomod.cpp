1daa72646661d488c51a37eed2f4172b891ffb47
Davis Kipchirchir <74677355+DavisKoreal@users.noreply.github.com>
2022-07-29 06:12:09 +0300

    Update hellomod.cpp

--- hellomod.cpp
+++ hellomod.cpp
@@ -1,11 +1,47 @@
-package hellomod
+package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
 
-func print_name() {
-	fmt.Println("Hello, man")
-}
+	"github.com/gocolly/colly"
+)
 
 func main() {
-	print_name()
+
+	fname := "data.csv"
+	file, err := os.Create(fname)
+	
+	if err != nil {
+		log.Fatalf("Could not create file, err : %q", err)
+		return
+	}
+
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	writer.Flush()
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("internshala.com"),
+	)
+
+	c.OnHTML(".internship_meta", func(e *colly.HTMLElement){
+		writer.Write([]string{
+			e.ChildText("a"),
+			e.ChildText("span"),
+
+		})
+	})
+
+	for i:= 1; i <= 150; i++{
+		fmt.Printf("Scraping page : %d\n", i)
+		c.Visit("http://internshala.com/internships/page-" + strconv.Itoa(i))
+	}
+
+	log.Printf("SCRAPING COMPLETE MAN \n")
+	log.Println(c)
 }
